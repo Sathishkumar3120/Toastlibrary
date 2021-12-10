0b180788e7c4e894b8cf00e9c84617c2c62c6dd2
sathish kumar <sathishkumar.s@indiumsoft.com>
2021-12-10 18:54:13 +0530

    Intial commit

--- app/src/main/java/com/kotlin/library/MainActivity.kt
+++ app/src/main/java/com/kotlin/library/MainActivity.kt
@@ -2,10 +2,14 @@ package com.kotlin.library
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.kotlin.mylibrary.ToastMessage
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        ToastMessage.s(applicationContext,"Hello world")
     }
 }
--- mylibrary/src/main/java/com/kotlin/mylibrary/ToastMessage.kt
+++ mylibrary/src/main/java/com/kotlin/mylibrary/ToastMessage.kt
@@ -6,9 +6,10 @@ import android.widget.Toast
 
 
 
-class ToastMessage {
-
-    fun s(c: Context?, message: String?) {
-        Toast.makeText(c, message, Toast.LENGTH_SHORT).show()
+public class ToastMessage {
+    companion object {
+        fun s(c: Context?, message: String?) {
+            Toast.makeText(c, message, Toast.LENGTH_SHORT).show()
+        }
     }
 }
